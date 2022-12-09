766d3a792080bc8c7332e2f9d257f42f1595026f
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2022-12-09 13:04:33 +0100

    hero Section

---
+++ Header/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'jakarta': ['Plus Jakarta Sans', 'sans-serif;'],
+        'inter': ['Inter', 'sans-serif;'],
+      },
+    },
+  },
+  plugins: [],
+}
